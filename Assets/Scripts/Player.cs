2e7579c2d47523ed6c5d4fdbb856376931197b10
Ally <star.goodman@gmail.com>
2017-08-24 13:06:56 -0600

    added two scenes, made everyone a folder

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Player : MonoBehaviour
+{
+	public Rigidbody rbody;
+	public float speed;
+
+	private float inputH;
+	private float inputV;
+
+
+	void Start ()
+	{
+		rbody = GetComponent<Rigidbody> ();
+	}
+
+
+	void Update ()
+	{
+		inputH = Input.GetAxis ("Horizontal");
+		inputV = Input.GetAxis ("Vertical");
+
+		float moveX = inputH * speed * Time.deltaTime;
+		float moveZ = inputV * speed * Time.deltaTime;
+
+		rbody.velocity = new Vector3 (moveX, 0, moveZ);
+	}
+}
---
+++ Assets/Scripts/Video.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Video : MonoBehaviour
+{
+	public MovieTexture movie;
+
+	// Use this for initialization
+	void Start ()
+	{
+		GetComponent<Renderer> ().material.mainTexture = movie;
+		movie.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
