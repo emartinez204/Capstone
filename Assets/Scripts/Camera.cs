9917db9894827c881e83b2c1f5233601dff328ca
Ally <star.goodman@gmail.com>
2017-09-04 14:56:06 -0600

    added placeholders

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+	public GameObject focus;
+	private Vector3 offset;
+
+
+	void Start ()
+	{
+		offset = transform.position - focus.transform.position;
+		offset.y += 3;
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.position = focus.transform.position + offset;
+
+	}
+}
---
+++ Assets/Scripts/Interactables.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class Interactables : MonoBehaviour
+{
+	public Text itemTxt;
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.tag == "Interactable") {
+			itemTxt.text = "This is a " + other.gameObject.name.ToString ();
+		}
+	}
+
+	void OnTriggerExit (Collider other)
+	{
+		itemTxt.text = "";
+	}
+}
